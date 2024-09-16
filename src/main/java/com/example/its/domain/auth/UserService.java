b7f6b0ddc846953923375d543d0f79a82215c97a
yamashun <yamashun.012538@gmail.com>
2024-09-16 17:33:35 +0900

    データベースから全データを取得する処理を実装

---
+++ src/main/java/com/example/its/domain/auth/UserService.java
@@ -0,0 +1,18 @@
+package com.example.its.domain.auth;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -1,19 +1,24 @@
 package com.example.its.web.user;
 
-import java.util.Collections;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.its.domain.auth.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping
     public String showList(Model model) {
-        model.addAttribute("userList", Collections.emptyList());
+        model.addAttribute("userList", userService.findAll());
         return "users/list";
     }
 }
