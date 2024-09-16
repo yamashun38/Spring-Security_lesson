f9e3c7a293c256a9512d5c9cadcd08c65eb963ee
yamashun <yamashun.012538@gmail.com>
2024-09-16 16:04:16 +0900

    データベースでユーザ情報を管理できるよう実装
    渡されたユーザ名がデータベースに登録されている値かどうか検索するメソッドを実装
    検索した結果、登録されていないユーザ名だった場合はエラーを返すよう実装
    仮データとして登録していたtomは削除

--- src/main/java/com/example/its/domain/auth/CustomUserDetailsService.java
+++ src/main/java/com/example/its/domain/auth/CustomUserDetailsService.java
@@ -1,23 +1,27 @@
 package com.example.its.domain.auth;
 
+import java.util.Collections;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        if("tom".equals(username)) {
-            return new CustomUserDetails("tom", "password", Collections.emptyList());
-        }
-        throw new UsernameNotFoundException(
-            "Given username is not found. (username = '" + username + "')"
-        );
+        return userRepository.findByUsername(username)
+                .map(user -> new CustomUserDetails(user.getUsername(), user.getPassword(), Collections.emptyList()))
+                .orElseThrow(() -> new UsernameNotFoundException(
+                    "Given username is not found. (username = '" + username + "')"
+                ));
     }
 }
