0a7ec2377f0336aa6cf005f1387018470fcb7601
yamashun <yamashun.012538@gmail.com>
2024-09-21 16:11:07 +0900

    ユーザー重複時のバリデーション処理を実装

---
+++ src/main/java/com/example/its/web/validation/UniqueUsernameValidator.java
@@ -0,0 +1,21 @@
+package com.example.its.web.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.example.its.domain.auth.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return userRepository.findByUsername(value).isEmpty();
+    }
+
+    
+}
