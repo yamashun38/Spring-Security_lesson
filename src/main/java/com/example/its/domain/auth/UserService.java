75606ed4519a9391ea13c4f98360fe409107a255
yamashun <yamashun.012538@gmail.com>
2024-09-25 16:10:55 +0900

    パスワードの文字数制限を追加 新規ユーザー登録時のパスワードエンコード処理を実装

--- src/main/java/com/example/its/domain/auth/UserService.java
+++ src/main/java/com/example/its/domain/auth/UserService.java
@@ -2,6 +2,7 @@ package com.example.its.domain.auth;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,12 +12,14 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     public void create(String username, String password) {
-        userRepository.insert(username, password);
+        var encodedPassword = passwordEncoder.encode(password);
+        userRepository.insert(username, encodedPassword);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,6 @@ insert into issues (summary, description) values ('バグA', 'バグがありま
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
 
-insert into users (username, password) values ('tom', 'password');
-insert into users (username, password) values ('bob', 'password');
+-- password1234
+insert into users (username, password) values ('tom', '08a49128a32d7f33c0f811282190de86e4012e6301d3262b15fc12c782c56bcd8e31c6b3d0ee1e78');
+insert into users (username, password) values ('bob', '1198be4b62a54243e2a62a23542aa39b525a00693a562fdd941bcfa73fd7c4087233f8fe5dfff305');
